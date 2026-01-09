bc252b885b856de694d72ce782e37098da1b1e28
qt-lester <lester@qualitytrade.com>
2026-01-09 09:54:45 +0800

    added time greet

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-function useNgarod(text) {
+function useNgarod(text, options) {
+  const { time = false } = options || {};
+
+  if (time) {
+    const hour = new Date().getHours();
+    let timeOfDay;
+
+    if (hour < 12) timeOfDay = "bigat"; // morning
+    else if (hour < 18) timeOfDay = "malem"; // afternoon
+    else timeOfDay = "rabii"; // evening
+
+    return `Naimbag a ${timeOfDay}, ` + text + " ngarod.";
+  }
+
   return text + " ngarod";
 }
 
